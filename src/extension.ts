ee81aa3c476b0f5f4e023e0789f38fd8ed695a2f
Fathony Mursyid <gusfathon.fm@gmail.com>
2025-07-27 00:13:34 +0700

    update

--- src/extension.ts
+++ src/extension.ts
@@ -9,7 +9,8 @@
 import * as vscode from "vscode";
 import * as path from 'path';
 import * as fs from 'fs';
-import { supabase } from './lib/supabaseClient';
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite';
 /*
  * Entry point
  * Don't Delete this code
@@ -26,21 +27,26 @@ export function activate(context: vscode.ExtensionContext) {
 
     // dashboard
     const disposableShowDashboard = vscode.commands.registerCommand('extension.showDashboard', async () => {
+        const db = await open({
+            filename: path.join(context.extensionPath, 'src', 'db', 'database.sqlite'),
+            driver: sqlite3.Database,
+        });
+
         const htmlPath = path.join(context.extensionPath, 'src', 'page', 'index.html');
+        const projects = await db.all('SELECT * FROM projects');
+        const files = await db.all('SELECT * FROM files');
+        const timelogs = await db.all('SELECT * FROM time_logs');
+
+        const htmlProjects = projects.map(p => `<li>${p.project_name}</li>`).join('');
+        const htmlFiles = files.map(f => `<li>${f.file_name} (${f.file_path})</li>`).join('');
+        const htmlTimeLogs = timelogs.map(t => `<li>${t.duration_seconds}s (${t.start_time})</li>`).join('');
+
         
         if (!fs.existsSync(htmlPath)) {
             vscode.window.showErrorMessage('html file not found');
             return;
         }
 
-        // supabase 
-        const { data, error } = await supabase.from('projects').select('*');
-        if (error) {
-            console.error(error);
-            vscode.window.showErrorMessage('failed to connect supabase');
-        } else {
-            console.log('Data project:', data);
-        }
         
         const uri = vscode.Uri.file(htmlPath);
         await vscode.env.openExternal(uri); 
