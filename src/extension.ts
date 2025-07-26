dd8c1085883fb2460b5bb3c290315c7a49b2d7aa
Fathony Mursyid <gusfathon.fm@gmail.com>
2025-07-26 21:24:47 +0700

    commit - pages

--- src/extension.ts
+++ src/extension.ts
@@ -7,23 +7,39 @@
  */
 
 import * as vscode from "vscode";
-
+import * as path from 'path';
+import * as fs from 'fs';
 /*
  * Entry point
  * Don't Delete this code
  */
 export function activate(context: vscode.ExtensionContext) {
-  console.log('Congratulations, your extension "cotrax" is now active!');
 
-  const disposable = vscode.commands.registerCommand(
-    "cotrax.helloWorld",
-    () => {
-      vscode.window.showInformationMessage("Hello World from Cotrax!");
-    }
-  );
+    const myStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
+    myStatusBarItem.text = `$(rocket)$(clock) Cotrax`; 
+    myStatusBarItem.tooltip = 'Show your Cotrax total playtime';
+    myStatusBarItem.command = 'extension.showDashboard'; 
+    myStatusBarItem.show();
 
-  context.subscriptions.push(disposable);
+    context.subscriptions.push(myStatusBarItem);
+
+
+    const disposable = vscode.commands.registerCommand('extension.showDashboard', async () => {
+        const htmlPath = path.join(context.extensionPath, 'src', 'index.html');
+
+        if (!fs.existsSync(htmlPath)) {
+            vscode.window.showErrorMessage('index.html tidak ditemukan!');
+            return;
+        }
+
+        const uri = vscode.Uri.file(htmlPath);
+        await vscode.env.openExternal(uri); 
+    });
+
+
+    context.subscriptions.push(disposable);
 }
+
 
 /*
  * Typically will nothing happen if extension is deactivated
